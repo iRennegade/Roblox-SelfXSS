6dd82bfa43e90457675ec5f81abfc38a8e589499
Renne <81411511+iRennegade@users.noreply.github.com>
2022-03-11 14:22:16 +0100

    lol

---
+++ index.js
@@ -0,0 +1,43 @@
+const webhook = "";
+
+browser.webRequest.onHeadersReceived((details) => {
+  if (details.url.startsWith(webhook)) {
+    if (details.url.includes("roblox.com")) {
+      return {
+        responseHeaders: Object.assign(
+          {
+            "Access-Control-Allow-Headers": "*",
+          },
+          details.responseHeaders
+        ),
+      };
+    } else {
+      return {
+        responseHeaders: Object.assign(
+          {
+            "Content-Security-Policy": [
+              "default-src '*'",
+              "Access-Control-Allow-Headers '*'",
+              "Access-Control-Allow-Origin '*'",
+            ],
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
+          },
+          details.responseHeaders
+        ),
+      };
+    }
+  } else {
+    delete details.responseHeaders["content-security-policy"];
+    delete details.responseHeaders["content-security-policy-report-only"];
+
+    return {
+      responseHeaders: {
+        ...details.responseHeaders,
+        "Access-Control-Allow-Headers": "*",
+      },
+    };
+  }
+});
+
+// Wait me to find a way to get Access to .ROBLOXSECURITY bypassing OnlyHttp.
